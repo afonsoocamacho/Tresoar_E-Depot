682fbe655af015f014ab7db22b6e07c28b122edd
Afonso <afonsocamacho62@gmail.com>
2024-11-26 12:56:02 +0100

    refactor views to use layout component; update about and home pages, and add contact page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <header>
-        <h1>About Us</h1>
-    </header>
-    <main>
-        <p>Welcome to our about page.</p>
-    </main>
-    <footer>
-        <p>&copy; {{ date('Y') }} Tresoar E-Depot. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+<x-layout>
+    <h1>About</h1>
+</x-layout>
