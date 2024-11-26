c17b1122df53164a25d41debe1316062c4c196c0
Afonso <afonsocamacho62@gmail.com>
2024-11-26 13:39:24 +0100

    enhance navigation accessibility and styling; add aria labels and update link styles

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,7 +1,8 @@
-<nav>
+<nav aria-label="Main Navigation">
+    <a href="https://tresoar.nl"><img src="" alt="Tresoar Logo" aria-label="Tresoar Home"></a>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/" aria-label="Home Page">Home</a></li>
+        <li><a href="/about" aria-label="About Page">About</a></li>
+        <li><a href="/contact" aria-label="Contact Page">Contact</a></li>
     </ul>
 </nav>
