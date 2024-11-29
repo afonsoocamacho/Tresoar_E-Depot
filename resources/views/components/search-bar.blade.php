60f46e9405067683ae516839ae12be728f330663
Afonso <afonsocamacho62@gmail.com>
2024-11-29 10:58:02 +0100

    add .DS_Store to track macOS Finder metadata

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,10 +1,15 @@
 <div class="search-bar">
     <div class="breadcrums"></div>
     <div class="search">
-        <div class="search-bar-input">
-         <form action="">
-        <input type="text" name=""  placeholder="Search..." id="">
-         </form>
+        <div class="search-form">
+            <form action="">
+                @csrf
+                <div class="search-input">
+                <button type="submit" aria-label="Search button"><div class="icon"><img src="{{ asset('assets/icons/magnifier.svg') }}" alt="magnifier"></div></button>
+                <input type="text" placeholder="Search..." aria-label="Search Bar">
+                
+                </div>
+            </form>
         </div>
     </div>
 </div>
