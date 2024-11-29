9eedf273dcd4b4f8aa75171d5d1b06825a977c49
reigler <92632760+reigler@users.noreply.github.com>
2024-11-29 16:10:06 +0100

    started card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <img src="{{ asset('assets/images/Rectangle_7.png') }}" alt="Card image" class="card-img-top">
+    <span class="card-icon"> <img src="{{ asset('assets/icons/heart.svg') }}" alt="heart"> </span>
     <div class="card-body">
-        <h5 class="card-title">card title</h5>
+        <h3 class="card-title">card title</h3>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <span class="card-icon"> <img src="{{ asset('assets/icons/heart.svg') }}" alt="heart"> </span>
     </div>
 </div>
