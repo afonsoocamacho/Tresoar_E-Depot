903f3b9124a35e110981fc98791b8f42320d4116
reigler <92632760+reigler@users.noreply.github.com>
2024-12-02 15:47:16 +0100

    removed unneccesary size constraints on card icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <div class="main-content">
         <x-search-bar></x-search-bar>
          <h1>HOME</h1>
+         <p class="suggested-search-filters">Recent searches</p>
          <x-card></x-card>
     </div>
 </x-layout>
