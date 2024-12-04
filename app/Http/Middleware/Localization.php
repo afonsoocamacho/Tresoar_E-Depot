88b2d29e32a8eb1f8d4b918f2f1f7449b1a7d64f
Afonso <afonsocamacho62@gmail.com>
2024-12-04 13:43:59 +0100

    localization fixed P1 - (Still to fix)

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -11,16 +11,36 @@ class Localization
 {
     /**
      * 
-     * --- Fix this -------!!!!!!!!!
-     * 
      * Handle an incoming request.
      * 
-     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next)
     {
-        App::setLocale($request->segment(1));
-        URL::defaults(['locale' => $request->segment(1)]);
-        return $next($request);
+        // Get the first segment of the URL
+        $locale = $request->segment(1);
+        
+        // Get the default locale from the config and the supported locales
+        $defaultLocale = config('app.locale');
+        $supportedLocales = ['en', 'nl', 'fy'];
+
+        // If no locale present in the URL, redirect to the locale stored in the cookies, if not in cookies, then use the default locale and store in cookies
+        if (!in_array($locale, $supportedLocales)) {
+            $locale = $request->cookie('locale', $defaultLocale);
+            return redirect($locale . '/' . $request->path());
+        }
+
+        // If the locale is supported, set it and store it in cookies
+        if (in_array($locale, $supportedLocales)) {
+            App::setLocale($locale);
+            URL::defaults(['locale' => $locale]);
+            $response = $next($request);
+            return $response->withCookie(cookie('locale', $locale, 43200)); // 43200 minutes = 30 days
+        }
+
+        // If the locale is not supported, abort with a 404 error
+        abort(404);
     }
 }
