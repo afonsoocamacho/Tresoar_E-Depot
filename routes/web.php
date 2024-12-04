f9664a7b763a8d4e8c4381e16306232d2de77ab7
Afonso <afonsocamacho62@gmail.com>
2024-12-04 18:03:25 +0100

    refactor localization routes and add home route

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,15 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\App;
 use App\Http\Middleware\Localization;
 
-//Fix this route fro the localization
- 
-Route::get('/', function () {
-    return redirect('en');
-});
-
 Route::prefix('{locale}') 
 ->middleware(Localization::class)
 ->group(function() {
 
     Route::get('/', function () {
+        return view('home');
+    });
+
+    Route::get('/home', function () {
         return view('home');
     });
     
