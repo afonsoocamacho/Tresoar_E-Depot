d233319a921056c9727ffc5b60b6efd504af191d
reigler <92632760+reigler@users.noreply.github.com>
2024-12-04 14:46:53 +0100

    trying to update the new hero banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,11 @@
 
 <x-layout>
     <div class="main-content">
-        <x-search-bar></x-search-bar>
-         <h1>HOME</h1>
+        <!-- <x-search-bar></x-search-bar> -->
+         <!-- <h1>HOME</h1> -->
+    </div>
+        <x-hero-banner></x-hero-banner>
+    <div class="main-content">    
          <x-cards-view text="Recent searches"></x-cards-view>
          <x-cards-view text="Popular topics"></x-cards-view>
         
