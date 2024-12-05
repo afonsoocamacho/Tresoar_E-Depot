49b57824817bddc99722f78c8a9a334bf6d3b475
reigler <92632760+reigler@users.noreply.github.com>
2024-12-05 10:56:31 +0100

    add government logo

--- resources/views/components/hero-banner.blade.php
+++ resources/views/components/hero-banner.blade.php
@@ -1,5 +1,6 @@
 <div class="hero-banner">
     <div class="hero-banner-text">
+        <img src="{{ asset('assets/icons/logo-digitale-overheid.svg') }}" alt="Logo Dutch Government">
         <h1>Find your perfect job</h1>
         <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
         <x-search-bar></x-search-bar>
