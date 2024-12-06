b05d212a8c311e6556d6c3d1d59c27d7e6386235
reigler <92632760+reigler@users.noreply.github.com>
2024-12-06 11:56:54 +0100

    changes raphi

--- resources/views/components/hero-banner.blade.php
+++ resources/views/components/hero-banner.blade.php
@@ -1,6 +1,6 @@
 <div class="hero-banner">
+    <img src="{{ asset('assets/icons/logo-digitale-overheid.svg') }}" alt="Logo Dutch Government">
     <div class="hero-banner-text">
-        <img src="{{ asset('assets/icons/logo-digitale-overheid.svg') }}" alt="Logo Dutch Government">
         <h1>{{__('hero.hero.title')}}</h1>
         <p>{{__('hero.hero.text')}}</p>
         <x-search-bar></x-search-bar>
