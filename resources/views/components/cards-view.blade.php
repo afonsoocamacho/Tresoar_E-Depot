58509a705fdfa99b750ffdfd82478cf8c22f5652
reigler <92632760+reigler@users.noreply.github.com>
2024-12-09 16:11:29 +0100

    fixed arrow and card posiitioning

--- resources/views/components/cards-view.blade.php
+++ resources/views/components/cards-view.blade.php
@@ -1,8 +1,13 @@
-<p class="suggested-search-filters" aria-label="{{$text}}">{{$text}}</p>
+<div class="filters-container">
+    <p class="suggested-search-filters" aria-label="{{$text}}">{{$text}}</p>
+    <div class="card-arrows">
+        <button class="card-arrow-left" aria-label="Previous"><img src="{{ asset('assets/icons/arrow-left.svg') }}" alt=""></button>
+        <button class="card-arrow-right" aria-label="Next"><img src="{{ asset('assets/icons/arrow-right.svg') }}" alt=""></button>
+    </div>
+</div>
 <div class="cards-view">
     <x-card text="Monumentsashcboasjbcoausbcsdkhvgksdjvb"></x-card>
     <x-card text="Leeuwarden"></x-card>
     <x-card text="Cadastral maps"></x-card>
     <x-card text="Leeuwarder newspaper"></x-card>
-   
 </div>
--- resources/views/components/hero-banner.blade.php
+++ resources/views/components/hero-banner.blade.php
@@ -1,8 +1,8 @@
 <div class="hero-banner">
     <img src="{{ asset('assets/icons/logo-digitale-overheid.svg') }}" alt="Logo Dutch Government">
     <div class="hero-banner-text">
-        <h1>{{__('hero.hero.title')}}</h1>
-        <p>{{__('hero.hero.text')}}</p>
+        <h1>{{__('all.hero.title')}}</h1>
+        <p>{{__('all.hero.text')}}</p>
         <x-search-bar></x-search-bar>
     </div>    
 </div>
