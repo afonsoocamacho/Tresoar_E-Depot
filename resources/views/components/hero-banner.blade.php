cbda1b6305af2bd6b7fc7b51d23f51a4c2190faf
reigler <92632760+reigler@users.noreply.github.com>
2024-12-11 14:50:56 +0100

    updated herobanner spacing and removed governmental logo

--- resources/views/components/hero-banner.blade.php
+++ resources/views/components/hero-banner.blade.php
@@ -1,5 +1,4 @@
 <div class="hero-banner">
-    <img src="{{ asset('assets/icons/logo-digitale-overheid.svg') }}" alt="Logo Dutch Government">
     <div class="hero-banner-text">
         <h1>{{__('all.hero.title')}}</h1>
         <p>{{__('all.hero.text')}}</p>
--- resources/views/components/metadata-view.blade.php
+++ resources/views/components/metadata-view.blade.php
@@ -1,4 +1,4 @@
 <div class="metadata-view">
     <h2 class="metadata-title">{!! wordwrap($text, 26, '<br>') !!}</h2>
 
-</div>
+</div> 
